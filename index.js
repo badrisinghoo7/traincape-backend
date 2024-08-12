b3d24f2ba9bd2f3c49a80d82a01fe38ab6a110ce
Badri Vishal Singh <123944508+badrisinghoo7@users.noreply.github.com>
2024-08-12 18:45:14 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
-
+// app.use(cors());
 
 app.use("/users", userRouter);
 app.use("/review", reviewRouter);
